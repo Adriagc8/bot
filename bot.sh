c597ceb8132a54a7d28874582bd4acc219a88888
Adria Gonzalez <adriagc8@pop-os.localdomain>
2021-02-08 17:36:12 +0100

    url

--- bot.sh
+++ bot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#https://class.ilern.es/bigbluebutton/api/join?fullName=User+1643814&lockSettingsDisableMic=true&meetingID=estres+&password=ap&redirect=true&checksum=6a86d9bdd80043f5484bc95ec94593fd81f720ac
 while getopts b:u: flag
 do
 case "${flag}" in
